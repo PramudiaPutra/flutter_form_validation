08ce6076ce62f28789216b76ef7e8db8b8e02b0e
pramudia_putra_pamungkas <pramudiaputra.iv@gmail.com>
2022-02-09 14:48:24 +0700

    add validation into bloc

--- lib/src/bloc/login_bloc.dart
+++ lib/src/bloc/login_bloc.dart
@@ -1,12 +1,19 @@
 import 'dart:async';
 
-class LoginBloc {
+import 'package:forms_validation/src/functions/form_validation.dart';
+
+class LoginBloc extends FormValidation {
   final _email = StreamController<String>();
   final _password = StreamController<String>();
 
-  get email => _email.stream;
-  get password => _password.stream;
+  get email => _email.stream.transform(validateEmail);
+  get password => _password.stream.transform(validatePassword);
 
   get changeEmail => _email.sink.add;
   get changePassword => _password.sink.add;
+
+  dispose() {
+    _email.close();
+    _password.close();
+  }
 }
