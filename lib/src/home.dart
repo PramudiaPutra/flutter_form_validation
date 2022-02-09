5dec9e60df423a870c51464afa12f664fa305483
pramudia_putra_pamungkas <pramudiaputra.iv@gmail.com>
2022-02-09 13:50:52 +0700

    refactor project

--- lib/src/home.dart
+++ lib/src/home.dart
@@ -1,19 +1,8 @@
-import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
-import 'package:forms_validation/src/functions/form_validation.dart';
+import 'package:forms_validation/src/screen/login_screen.dart';
 
-class Home extends StatefulWidget {
+class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
-
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> with FormValidation {
-  final formKey = GlobalKey<FormState>();
-
-  String? email = '';
-  String? passwd = '';
 
   @override
   Widget build(BuildContext context) {
@@ -21,56 +10,7 @@ class _HomeState extends State<Home> with FormValidation {
       appBar: AppBar(
         title: const Text('Login'),
       ),
-      body: Container(
-        margin: const EdgeInsets.all(20.0),
-        child: Form(
-          key: formKey,
-          child: Column(
-            children: [
-              emailField(),
-              passwordField(),
-              const SizedBox(height: 12),
-              submitButton(),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget emailField() {
-    return TextFormField(
-      keyboardType: TextInputType.emailAddress,
-      decoration: const InputDecoration(
-        labelText: 'Email',
-        hintText: 'usename@email.com',
-      ),
-      validator: validateEmail,
-      onSaved: (newValue) => email = newValue,
-    );
-  }
-
-  Widget passwordField() {
-    return TextFormField(
-      obscureText: true,
-      decoration: const InputDecoration(
-        labelText: 'Password',
-        hintText: 'Password',
-      ),
-      validator: validatePassword,
-      onSaved: (newValue) => passwd = newValue,
-    );
-  }
-
-  Widget submitButton() {
-    return ElevatedButton(
-      child: const Text('Submit'),
-      onPressed: () {
-        if (formKey.currentState!.validate()) {
-          formKey.currentState!.save();
-          print('post email $email and password $passwd to remote api');
-        }
-      },
+      body: const LoginScreen(),
     );
   }
 }
---
+++ lib/src/screen/login_screen.dart
@@ -0,0 +1,49 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          emailField(),
+          passwordField(),
+          const SizedBox(height: 12),
+          submitButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: const InputDecoration(
+        labelText: 'Email',
+        hintText: 'usename@email.com',
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    return TextFormField(
+      obscureText: true,
+      decoration: const InputDecoration(
+        labelText: 'Password',
+        hintText: 'Password',
+      ),
+    );
+  }
+
+  Widget submitButton() {
+    return ElevatedButton(
+      child: const Text('Submit'),
+      onPressed: () {},
+    );
+  }
+}
